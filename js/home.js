a2edc04a930dcbc7bbaec5d13e66b98fff87bed9
ahmed elmogy <ahmed.elmogy2005@gmail.com>
2024-10-05 03:01:12 +0300

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -5,10 +5,10 @@ let nameHome=document.querySelector(".nameHome")
 let emails=JSON.parse(localStorage.getItem("emails"))
 //^ Event
 logOut.addEventListener("click",()=>{
-   nameHome.innerHTML=emails[emails.length-1].userName
     location.href="/log-in/index.html";
 })
 
+nameHome.innerHTML=emails[emails.length-1].userName
 
 if(window.location.pathname ==="/pageHome.html"){
     nameHome.innerHTML=emails[emails.length-1].userName
